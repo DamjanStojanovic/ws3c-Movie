d6a8b063350dc478d6e2ca3003ad5f44efa0fe80
Marco Worni <marco.worni@students.fhnw.ch>
2024-05-28 17:44:29 +0200

    just show films with img and title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -33,30 +33,35 @@ function AppContent() {
           </Routes>
       ) : (
           <div className="container">
-            <section className="row colset-2-its">
-              <form id="searchForm" onSubmit={searchMovies}>
-                <div style={{ position: 'relative', display: 'inline-block' }}>
-                  <input type="text" id="searchInput" placeholder="Search for movies..." style={{ paddingRight: '60px' }} value={query} onChange={(e) => setQuery(e.target.value)} />
-                  <button type="submit" id="searchButton" style={{ position: 'absolute', right: 0, top: 0 }}>
-                    &#x1F50E;&#xFE0E;
-                  </button>
-                </div>
-              </form>
-              <div id="results">
-                {movies.map((movie) => (
-                    <div key={movie.id}>
-                      <Link to={`movie/${movie.id}`}>
-                        <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w200${movie.poster_path}` : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg'} alt={movie.title} data-movie-id={movie.id} />
-                      </Link>
-                      <p>{movie.title}</p>
-                    </div>
-                ))}
-              </div>
-            </section>
-            <Routes>
-              <Route path="/new-page" element={<NewPage />} />
-              <Route path="/movie/:id" element={<MovieDetail />} />
-            </Routes>
+              <section className="row colset-2-its">
+                  <form id="searchForm" onSubmit={searchMovies}>
+                      <div style={{position: 'relative', display: 'inline-block'}}>
+                          <input type="text" id="searchInput" placeholder="Search for movies..."
+                                 style={{paddingRight: '60px'}} value={query}
+                                 onChange={(e) => setQuery(e.target.value)}/>
+                          <button type="submit" id="searchButton" style={{position: 'absolute', right: 0, top: 0}}>
+                              &#x1F50E;&#xFE0E;
+                          </button>
+                      </div>
+                  </form>
+                  <div id="results">
+                      {movies.map((movie) => (
+                          movie.poster_path && movie.title && movie.title.trim() !== '' ? (
+                              <div key={movie.id}>
+                                  <Link to={`movie/${movie.id}`}>
+                                      <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}
+                                           data-movie-id={movie.id}/>
+                                  </Link>
+                                  <p>{movie.title}</p>
+                              </div>
+                          ) : null
+                      ))}
+                  </div>
+              </section>
+              <Routes>
+                  <Route path="/new-page" element={<NewPage/>}/>
+                  <Route path="/movie/:id" element={<MovieDetail/>}/>
+              </Routes>
           </div>
       )
   );
