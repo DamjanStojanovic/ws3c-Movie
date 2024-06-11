dcfb9e7209a4854d282bb162d495a04cb4820170
DamjanStojanovic <damjan.stojanovic@students.fhnw.ch>
2024-06-11 16:10:45 +0200

    add watchlist button in navbar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,19 +1,27 @@
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Search from "./Search";
 
 const Navbar = () => {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-  return (
-      <nav className="navbar glassmorphism-effect">
-        <Link to="/" className="navbar-brand">
-          <img src={`${process.env.PUBLIC_URL}/MovieDB-Logo_mobile.png`} alt="MovieDB" className="logo mobile-image" />
-          <img src={`${process.env.PUBLIC_URL}/MovieDB-Logo_desktop.png`} alt="MovieDB" className="logo desktop-image" />
-        </Link>
-        <Search setMovies={setMovies}/>
-      </nav>
-  );
+    return (
+        <nav className="navbar glassmorphism-effect">
+            <Link to="/" className="navbar-brand">
+                <img src={`${process.env.PUBLIC_URL}/MovieDB-Logo_mobile.png`} alt="MovieDB"
+                     className="logo mobile-image"/>
+                <img src={`${process.env.PUBLIC_URL}/moviedb_logo_desktop.svg`} alt="MovieDB"
+                     className="logo desktop-image"/>
+            </Link>
+            <Link to="/watchlist" className="navbar-brand">
+                <img src={`${process.env.PUBLIC_URL}/watchlist_mobile.png`} alt="Watchlist"
+                     className="logo mobile-image"/>
+                <img src={`${process.env.PUBLIC_URL}/watchlist_desktop.png`} alt="Watchlist"
+                     className="logo desktop-image"/>
+            </Link>
+            <Search setMovies={setMovies}/>
+        </nav>
+    );
 };
 
 export default Navbar;
