b1dca6368bca8bcac481229bbfd9f189625b65dd
Andrinsibold <andrin.sibold@students.fhnw.ch>
2024-06-11 14:21:00 +0200

    reworked home page to add carousel, css desgin with glassmorphism effect

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {Link, Route, Routes, useLocation} from "react-router-dom";
 import {fetchData} from "../api/api";
 import MovieDetail from "./MovieDetail";
+import MovieCarousel from "./MovieCarousel";
 
 function Home() {
     const [popularMovies, setPopularMovies] = useState([]);
@@ -24,57 +25,64 @@ function Home() {
     }, []);
 
     return (
-        location.pathname.startsWith('/movie/') ? (
-            <Routes>
-                <Route path="/movie/:id" element={<MovieDetail />} />
-            </Routes>
-        ) : (
-            <div className="container">
-                <section>
-                    <h2>Popular Movies</h2>
-                    <div id="popular-movies">
-                        {popularMovies.map((movie) => (
-                            movie.poster_path && movie.title && movie.title.trim() !== '' ? (
-                                <div className="movie">
-                                    <div key={movie.id} className="result-item">
-                                        <Link to={`movie/${movie.id}`} className="movieTitle">
-                                            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                                                alt={movie.title}
-                                                data-movie-id={movie.id}/>
-                                        </Link>
-                                    </div>
-                                    <Link to={`movie/${movie.id}`} className="movieTitle">
-                                        <p>{movie.title}</p>
-                                    </Link>
-                                </div>
-                            ) : null
-                        ))}
-                    </div>
-                    <div>
-                        <h2>Upcoming Movies</h2>
-                        <div id="upcoming-movies">
-                            {upcomingMovies.map((movie) => (
-                                movie.poster_path && movie.title && movie.title.trim() !== '' ? (
-                                    <div className="movie">
-                                        <div key={movie.id} className="result-item">
-                                            <Link to={`movie/${movie.id}`} className="movieTitle">
-                                                <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                                                    alt={movie.title}
-                                                    data-movie-id={movie.id}/>
-                                            </Link>
-                                        </div>
-                                        <Link to={`movie/${movie.id}`} className="movieTitle">
-                                            <p>{movie.title}</p>
-                                        </Link>
-                                    </div>
-                                ) : null
-                            ))}
-                        </div>
-                    </div>
-                </section>
-            </div>
-        )
+        <div className="container">
+            <MovieCarousel title="Trending Movies" movies={popularMovies}/>
+            <MovieCarousel title="Upcoming Movies" movies={upcomingMovies}/>
+        </div>
     );
+
+    // return (
+    //     location.pathname.startsWith('/movie/') ? (
+    //         <Routes>
+    //             <Route path="/movie/:id" element={<MovieDetail />} />
+    //         </Routes>
+    //     ) : (
+    //         <div className="container">
+    //             <section>
+    //                 <h2>Popular Movies</h2>
+    //                 <div id="popular-movies">
+    //                     {popularMovies.map((movie) => (
+    //                         movie.poster_path && movie.title && movie.title.trim() !== '' ? (
+    //                             <div className="movie">
+    //                                 <div key={movie.id} className="result-item">
+    //                                     <Link to={`movie/${movie.id}`} className="movieTitle">
+    //                                         <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+    //                                             alt={movie.title}
+    //                                             data-movie-id={movie.id}/>
+    //                                     </Link>
+    //                                 </div>
+    //                                 <Link to={`movie/${movie.id}`} className="movieTitle">
+    //                                     <p>{movie.title}</p>
+    //                                 </Link>
+    //                             </div>
+    //                         ) : null
+    //                     ))}
+    //                 </div>
+    //                 <div>
+    //                     <h2>Upcoming Movies</h2>
+    //                     <div id="upcoming-movies">
+    //                         {upcomingMovies.map((movie) => (
+    //                             movie.poster_path && movie.title && movie.title.trim() !== '' ? (
+    //                                 <div className="movie">
+    //                                     <div key={movie.id} className="result-item">
+    //                                         <Link to={`movie/${movie.id}`} className="movieTitle">
+    //                                             <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+    //                                                 alt={movie.title}
+    //                                                 data-movie-id={movie.id}/>
+    //                                         </Link>
+    //                                     </div>
+    //                                     <Link to={`movie/${movie.id}`} className="movieTitle">
+    //                                         <p>{movie.title}</p>
+    //                                     </Link>
+    //                                 </div>
+    //                             ) : null
+    //                         ))}
+    //                     </div>
+    //                 </div>
+    //             </section>
+    //         </div>
+    //     )
+    // );
 }
 
 export default Home;
---
+++ my-app/src/components/MovieCarousel.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import '../styles/MovieCarousel.css';
+import {Link} from "react-router-dom";
+
+/*
+  This component is a carousel that displays a list of movies.
+  The movies are passed as props to the component.
+  The component uses the react-multi-carousel library to create the carousel.
+  See the documentation here: https://react-multi-carousel.surge.sh/
+ */
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+    slidesToSlide: 5
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+    slidesToSlide: 2
+  }
+};
+
+const MovieCarousel = ({ title, movies }) => {
+  return (
+      <div className="movie-carousel-wrapper">
+        <h2>{title}</h2>
+        <div className="movie-carousel glassmorphism-effect">
+          <Carousel responsive={responsive}
+                    slidesToSlide={responsive.slidesToSlide}
+                    autoPlay
+                    autoPlaySpeed={5000}
+                    pauseOnHover
+                    rewind={true}
+                    rewindWithAnimation={true}
+                    swipeable
+                    centerMode={true}
+          >
+            {movies.map(movie => (
+                <div className="movie-card" key={movie.id}>
+                  <Link to={`/movie/${movie.id}`}>
+                    <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
+                    <h3>{movie.title}</h3>
+                  </Link>
+                </div>
+            ))}
+          </Carousel>
+        </div>
+      </div>
+  );
+};
+
+export default MovieCarousel;
